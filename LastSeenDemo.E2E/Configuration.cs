d2b3a314210ca81d3d724177d0482915b083bec7
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-10-17 22:06:28 +0300

    update BaseUrl

--- LastSeenDemo.E2E/Configuration.cs
+++ LastSeenDemo.E2E/Configuration.cs
@@ -3,7 +3,17 @@
 public static class Configuration
 {
     private const string DefaultUrl = "http://127.0.0.1:5079";
-    
-    public static string
-      BaseUrl => Environment.GetEnvironmentVariable("VM_IP") ?? DefaultUrl;
+
+    public static string BaseUrl
+    {
+        get
+        {
+            var env = Environment.GetEnvironmentVariable("VM_IP");
+            if (env != null)
+            {
+                return $"http://{env}";
+            }
+            return DefaultUrl;
+        }
+    }
 }
